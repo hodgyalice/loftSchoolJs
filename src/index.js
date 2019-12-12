fd4aad0b43590c0b9fa3b1ce5e1e5e50ad0f293a
hodgyalice <50991954+hodgyalice@users.noreply.github.com>
2019-12-12 18:24:11 +0100

    newExercise

--- src/index.js
+++ src/index.js
@@ -16,25 +16,41 @@ function returnFirstArgument(arg) {
  Значение по умолчанию второго аргумента должно быть 100
  */
 function defaultParameterValue(a, b) {
+	b = 100;
 	return a + b;
 }
 
+defaultParameterValue()
 /*
  Задание 3:
 
  Функция должна возвращать все переданные в нее аргументы в виде массива
  Количество переданных аргументов заранее неизвестно
  */
-function returnArgumentsArray() {
+function returnArgumentsArray(arguments) {
+	for (i = 0; i < arguments.length; i++) {
+		return [arguments];
+	}
 }
 
+returnArgumentsArray()
 /*
  Задание 4:
 
  Функция должна принимать другую функцию и возвращать результат вызова переданной функции
  */
 function returnFnResult(fn) {
+	return fn;
 }
+
+function otherFn(var1, var2) {
+	return var1(var2);
+}
+
+otherFn(returnFnResult)
+
+
+
 
 /*
  Задание 5:
